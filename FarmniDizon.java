e6cbab36046f6c6dc1fd34dbd63cbe1d6c3b6f7c
EvapeGT <141211210+EvapeGT@users.noreply.github.com>
2023-10-11 17:26:06 +0800

    Add files via upload

---
+++ FarmniDizon.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class FarmniDizon {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dozen = 0;
+        double loose = 0;
+        System.out.println(
+                "============================== \n"
+                + " Welcome to Dizon Dairy Farms \n"
+                + "============================== ");
+        System.out.print("Enter number of eggs: ");
+        int egg = Integer.valueOf(sc.nextLine());
+        if (egg < 0) {
+            System.out.println("Invalid input. Please enter a non-negative number of eggs.");
+        } else {
+            System.out.println("==============================");
+            System.out.println("You ordered " + egg + " eggs");
+            if (egg >= 12) {
+                dozen += 1;
+                egg -= 12;
+            }
+            loose = egg;
+
+            double price = (dozen * 36) + (loose * 4.75);
+            System.out.println("No. of dozen: " + dozen + " at Php 36.00 per dozen\n"
+                    + "No. of loose eggs: " + egg + " at Php 4.75 each.\n"
+                    + "==============================\n"
+                    + "Your Total Amount Due is: " + price + "\n"
+                    + "==============================");
+
+        }
+    }
+}
